77c9f6be6853c127528864c5b92e308bdc0b67b1
Nandkishor <Nandkishor@DESKTOP-5CJCDRV>
2020-07-31 07:56:22 +0530

    push the changes

---
+++ registration-services/src/main/java/com/myride/registration/swagger/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.myride.registration.swagger.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value ="/")
+	public String index() {
+		return "redirect:swagger-ui.html";
+		
+	}
+
+}
