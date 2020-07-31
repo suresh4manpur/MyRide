77236b68f8fa656fafad8cb54371d1a1a0793218
Nandkishor <Nandkishor@DESKTOP-5CJCDRV>
2020-07-31 08:48:26 +0530

    swagger implemnted in notification mail

--- notification-services/src/main/java/com/myride/notification/controller/NotificationController.java
+++ notification-services/src/main/java/com/myride/notification/controller/NotificationController.java
@@ -2,6 +2,7 @@ package com.myride.notification.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,18 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.myride.common.exception.MyRideException;
 import com.myride.notification.model.NotificationDetails;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
+@CrossOrigin(origins ="*",allowedHeaders = "*")
+@Api(value ="/v1" , tags = {"Notification APIs"})
 public class NotificationController {
-
+	//@ApiOperation(value = "Register my ride cab",nickname = "registerService" , notes = "Register my ride cab",response = String.class,tags = {"Register APIs"})
 	@GetMapping(value = "/")
 	public String hello() {
 		return "Hello from notification services!";
 	}
-
-	@PostMapping("/v1/send-mail")
+	@ApiOperation(value = "Send mail ride cab",nickname = "sendMail" , notes = "Send mail ride cab",response = String.class,tags = {"Notification APIs"})
+	@PostMapping("/send-mail")
 	public ResponseEntity<String> sendNotification(@RequestBody NotificationDetails cabRegistrationNotification) {
 		log.info("Sending the mail - " + cabRegistrationNotification);
 		return new ResponseEntity<String>(HttpStatus.OK);
---
+++ notification-services/src/main/java/com/myride/registration/swagger/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.myride.registration.swagger.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value ="/")
+	public String index() {
+		return "redirect:swagger-ui.html";
+	}
+
+}
